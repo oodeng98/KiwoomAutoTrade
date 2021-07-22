d07ee5da45634d5e2db92ff6026f05f45b0774cf
Hepheir-ZenBook <hepheir@gmail.com>
2021-07-22 14:27:56 +0900

    코드 하단에 __main__ 분기 생성

--- kiwoom_auto_trade/__main__.py
+++ kiwoom_auto_trade/__main__.py
@@ -1,3 +1,9 @@
+import json
+import sys
+
+from PyQt5.QtCore import QEventLoop
+from PyQt5.QtWidgets import QApplication
+
 from kiwoom_auto_trade.api import KiwoomOpenAPI
 from kiwoom_auto_trade.db import Database
 
@@ -6,3 +12,29 @@ class KiwoomAutoTrade(KiwoomOpenAPI):
     def __init__(self):
         self.db = Database()
         super().__init__()
+
+
+if __name__ == "__main__":
+    with open('config.json', 'r', encoding='utf-8') as f:
+        config = json.loads(f.read())
+
+    summary = {
+        '시작금액': '',
+        '종료금액': '',
+    }
+
+    app = QApplication(sys.argv)
+
+    kat = KiwoomAutoTrade()
+    kat.comm_connect()
+
+    try:
+        summary['시작금액'] = kat.d2_deposit
+
+        kat.set_input_value("계좌번호", config["계좌번호"])
+        kat.event_loop = QEventLoop()
+        kat.event_loop.exec()
+    except Exception:
+        pass
+    finally:
+        summary['종료금액'] = kat.d2_deposit
