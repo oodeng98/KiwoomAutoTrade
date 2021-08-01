58132b36e8684de277b50c24e3bd57fd7c543b0f
Hepheir <hepheir@gmail.com>
2021-08-01 22:04:37 +0900

    minor change

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,34 +1,39 @@
+from sys import argv
 import json
-import sys
 
 from PyQt5.QtWidgets import QApplication
 
 from bot import AutoTradingBot
 
 
-with open('config.json', 'r', encoding='utf-8') as f:
-    config = json.loads(f.read())
+def main():
+    with open('config.json', 'r', encoding='utf-8') as f:
+        config = json.loads(f.read())
 
-summary = {
-    '시작금액': '',
-    '종료금액': '',
-}
+    summary = {
+        '시작금액': '',
+        '종료금액': '',
+    }
 
-app = QApplication(sys.argv)
-bot = AutoTradingBot()
+    app = QApplication(argv)
+    bot = AutoTradingBot()
 
-# initial_capital = bot.balance  # 초기자본
+    # initial_capital = bot.balance  # 초기자본
 
-# print('시작금액:', bot.balance)
-# print('종료금액:', bot.balance)
+    # print('시작금액:', bot.balance)
+    # print('종료금액:', bot.balance)
 
-# try:
-#     summary['시작금액'] = bot.d2_deposit
+    # try:
+    #     summary['시작금액'] = bot.d2_deposit
 
-#     bot.set_input_value("계좌번호", config["계좌번호"])
-#     bot.event_loop = QEventLoop()
-#     bot.event_loop.exec()
-# except Exception:
-#     pass
-# finally:
-#     summary['종료금액'] = bot.d2_deposit
+    #     bot.set_input_value("계좌번호", config["계좌번호"])
+    #     bot.event_loop = QEventLoop()
+    #     bot.event_loop.exec()
+    # except Exception:
+    #     pass
+    # finally:
+    #     summary['종료금액'] = bot.d2_deposit
+
+
+if __name__ == '__main__':
+    main()
