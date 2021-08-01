a1b18242d4e5e77ac36dcf9fedc3b07d0e5abfd1
Hepheir-ZenBook <hepheir@gmail.com>
2021-08-01 14:11:52 +0900

    주식 체결 될 때마다 실행할 함수 생성 / 종료 타이머 손봄

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,5 +1,8 @@
 
 
+from datetime import date, datetime
+from typing import Optional, Union
+
 from PyQt5.QtCore import QEventLoop
 
 from bot.db import Database
@@ -7,44 +10,73 @@ from kiwoom import KiwoomOpenAPI
 
 
 class AutoTradingBot(KiwoomOpenAPI):
-    def __init__(self) -> None:
+    def __init__(self, closing: Optional[date] = None) -> None:
+        """자동 주식 매매 봇을 실행시킵니다.
+
+        Args:
+            closing: (Optional; datetime) 주식시장이 닫히는 시간. 이 시간에 맞춰 주식 자동매매가 종료됩니다.
+        """
         super().__init__()
 
         self.db = Database()
-        self._balance: int = 0
 
-        # Login
+        self._balance: int = 0
+        self.closing: Union[date, None] = closing
+
+        self.login()
+        self.start()
+
+    def login(self) -> None:
+        """키움 증권 API로 로그인"""
         self.CommConnect()
 
+    def start(self) -> None:
+        """자동 주식 매매 봇을 시작합니다."""
         # Set Event Listners
         self.OCXconn.OnEventConnect.connect(self.OnEventConnect)
         self.OCXconn.OnReceiveRealData.connect(self.OnReceiveRealData)
         self.OCXconn.OnReceiveTrData.connect(self.OnReceiveTrData)
         self.OCXconn.OnReceiveChejanData.connect(self.OnReceiveChejanData)
+        # Start event
+        self.event_loop = QEventLoop()
+        self.event_loop.exec()
 
+    def stop(self, *args, **kwargs) -> None:
+        """자동 주식 매매 봇을 정지시킵니다."""
+        self.event_loop.exit()
 
     # Override
+
     def OnEventConnect(self, nErrCode: int) -> None:
         if nErrCode == 0:
             print('로그인 성공')
         else:
             print('로그인 실패')
 
-
     # Override
+
     def OnReceiveRealData(self, sJongmokCode: str, sRealType: str, sRealData: str) -> None:
+        if self.closing is not None:
+            # 현재 시간이 self.closing에 지정된 시간보다 이후이면 프로그램 종료
+            now = datetime.now()
+            if now.toordinal() >= self.closing.toordinal():
+                self.stop()
+                return
         if sRealType != '주식체결':
-            price = int(self.GetCommRealData(sJongmokCode, 10)) # 가격
-            count = int(self.GetCommRealData(sJongmokCode, 15)) # 거래량
+            self.on_stock_execution(sJongmokCode, sRealType, sRealData)
 
+    def on_stock_execution(self, sJongmokCode: str, sRealType: str, sRealData: str) -> None:
+        """주식 체결 될 때마다 실행 될 함수
 
-
+        `OnReceiveRealData()`함수에서 이 함수가 호출 됨.
+        """
+        price = int(self.GetCommRealData(sJongmokCode, 10))  # 가격
+        count = int(self.GetCommRealData(sJongmokCode, 15))  # 거래량
 
     @property
     def balance(self) -> int:
         """현재 계좌잔고"""
         return self._balance
-
 
     def _receive_chejan_data(self, gubun, item_cnt, fid_list):
         order_number = int(self.get_chejan_data(FID.StockQuote.order_number))
